c247cbf667b3c13dd5e40eef9309e6db18ce649c
Samuel Dwomoh <sdwomoh418@gmail.com>
2026-02-05 03:05:17 +0000

    auth

--- auth_routes.py
+++ auth_routes.py
@@ -3,6 +3,7 @@ from database import Session, engine
 from schemas import SignUpModel
 from models import Usser
 from fastapi.exceptions import HTTPException
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 auth_router = APIRouter(
@@ -16,9 +17,16 @@ session=Session(bind=engine)
 async def hello():
     return {"message": "Hello world"}
 
-@auth_router.post("/signup")
+@auth_router.post("/signup", response_model=SignUpModel,status_code=status.HTTP_201_CREATED)
 async def signup(user:SignUpModel):
-    db_email=session.query(User).filter(User.email=user.email).first()
+    db_email=session.query(User).filter(User.email==user.email).first()
     if db_email is not None:
         return HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User with the email already exists")
     
+    db_username=session.query(User).filter(User.email==user.email).first()
+    if db_username is not None:
+        return HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User with the username already exists")
+    new_user=User(username=user.username, email=user.email, password=generate_password_hash(user.password),is_active=user.is_active, is_staff=user.is_staff)
+    session.add(new_user)
+    session.commit()
+    return new_user
