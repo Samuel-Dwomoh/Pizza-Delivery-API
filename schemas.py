f0f61ba8a7d4b3a13bf1d70bb50c69861ec385cd
Samuel Dwomoh <sdwomoh418@gmail.com>
2026-02-15 19:03:33 +0000

    jwt_secret_key

--- schemas.py
+++ schemas.py
@@ -13,3 +13,17 @@ class SignUpModel(BaseModel):
     class Config:
         orm_mode=True
         schema_extra={'example':{ "username": "johndoe", "email":"johndoe@gmail.com", "is_staff": False,"is_active": True}}
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    class Settings(BaseModel):
+        authjwt_secret_key: str = '6ed121c0d717b01a26eb359ca2dec5a59ec1acf457ef299b7b211a5e50008f7a'
